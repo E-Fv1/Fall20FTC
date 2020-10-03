0825ceadbf1d65e4bce2887d38a93c87973d79f5
E-Fv1 <theedisonfuh@gmail.com>
2020-10-03 11:56:15 -0700

    added conveyor and started drive code

---
+++ HERO Simple Application1/Conveyor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+
+namespace HERO_Simple_Application1
+{
+
+    public enum ConveyorStates
+    {
+        STOP_STATE,
+        IN_STATE,
+        OUT_STATE
+    }
+
+    public class Conveyor
+    {
+
+        private int conveyorTalonID = 0;
+
+        private int conveyorState = (int) ConveyorStates.STOP_STATE;
+        
+        public Conveyor()
+        {
+            CTRE.Phoenix.MotorControl.CAN.TalonSRX conveyorTalon = new CTRE.Phoenix.MotorControl.CAN.TalonSRX(conveyorTalonID);
+        }
+
+        public void stateMachine()
+        {
+            switch (conveyorState)
+            {
+                case (int) ConveyorStates.STOP_STATE:
+                    break;
+                case (int) ConveyorStates.IN_STATE:
+                    break;
+                case (int) ConveyorStates.OUT_STATE:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Stop()
+        {
+
+        }
+
+        private void In()
+        {
+
+        }
+
+        private void Out()
+        {
+
+        }
+    }
+}
--- HERO Simple Application1/Program.cs
+++ HERO Simple Application1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using CTRE.Phoenix.MotorControl;
 using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
 
@@ -7,11 +8,35 @@ namespace HERO_Simple_Application1
 {
     public class Program
     {
+
+
+        private int driveTalon1ID = 0;
+        private int driveTalon2ID = 0;
+
         public static void Main()
         {
             //robotInit();
+            int driveTalonNeutralMode = 0; // 0 Coast -=- 1 Brake
+
+            CTRE.Phoenix.MotorControl.CAN.TalonSRX driveTalon1 = new CTRE.Phoenix.MotorControl.CAN.TalonSRX(0);
+            CTRE.Phoenix.MotorControl.CAN.TalonSRX driveTalon2 = new CTRE.Phoenix.MotorControl.CAN.TalonSRX(0);
+
+            switch (driveTalonNeutralMode)
+            {
+                case 0:
+                    driveTalon1.SetNeutralMode(NeutralMode.Coast);
+                    driveTalon2.SetNeutralMode(NeutralMode.Coast);
+                    break;
+                case 1:
+                    driveTalon1.SetNeutralMode(NeutralMode.Brake);
+                    driveTalon2.SetNeutralMode(NeutralMode.Brake);
+                    break;
+                default:
+                    break;
+            }
 
             Intake intake = new Intake();
+            Conveyor conveyor = new Conveyor();
 
             /* simple counter to print and watch using the debugger */
             int counter = 0;
